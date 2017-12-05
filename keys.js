3fbb4f9da4e5b95ed407b59c240114352617a30b
Paul Zavattieri <paulz92@gmail.com>
2017-12-05 15:32:12 -0500

    omdb functionality

--- keys.js
+++ keys.js
@@ -1,4 +1,6 @@
-console.log('this is loaded');
+// commenting this out - it was in our instructions not to touch it but I don't see the purpose
+// will remove entirely after grading and when I add it to my portfolio
+// console.log('this is loaded');
 
 var twitterKeys = {
   consumer_key: 'Gf3ynl4rsVEEmdtff4dAs7Dpt',
@@ -7,4 +9,17 @@ var twitterKeys = {
   access_token_secret: 'QRoNAwFAa1Km8hwTjP70fR0ZBDI7ZDcg1Rcq14HsJGysF',
 }
 
-module.exports = twitterKeys;
+var spotifyKeys = {
+	id: "7bc14062ba5d492f9df0018efa9d9a03", 
+	secret: "c71061d147bf498da063e6711270efcd",
+}
+
+var omdbKey = {
+	key: "a0f5c7cc",
+}
+
+module.exports = {
+	twitterKeys: twitterKeys,
+	spotifyKeys: spotifyKeys,
+	omdbKey: omdbKey
+};
